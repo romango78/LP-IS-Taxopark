e973f8565b12f15aa22e879a1a6cf70b3fe79057
Nataliia Tkachenko <60783800+natatkachenko@users.noreply.github.com>
2022-02-22 16:08:24 +0200

    Add MySQL connection and docker-compose settings (#15)
    * add webapi
    
    * add angular app
    
    * add starting settings for angular and webapi
    
    * add publish settings for webapi
    
    * add docker support, move dockerfile to the folder with .sln
    
    * add commands of nodejs installation by settings in .csproj
    
    * Delete .vs directory
    
    * Delete Taxopark/.vs directory
    
    * Delete Taxopark/Taxopark/bin directory
    
    * Delete Taxopark/Taxopark/obj directory
    
    * Delete Taxopark.csproj.user
    
    * edit .gitignore by visual studio pattern
    
    * Delete package-lock.json
    
    * add mysql connection and docker-compose settings
    
    * change ConfigureMySQLContext to ConfigureDBContext
    
    * fix error "Invalid version: 15.2-15.3" in Angular project
    
    * add script for COPY command to recreate the folder structure
    
    * remove user password in the connection string
    
    Co-authored-by: Nataliia Tkachenko <nataliia_tkachenko1@epam.com>

---
+++ Taxopark/Taxopark.Infrastructure.Data/TaxoparkContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taxopark.Infrastructure.Data
+{
+    public class TaxoparkContext : DbContext
+    {
+        public TaxoparkContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
